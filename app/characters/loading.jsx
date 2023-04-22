b5dc48527bbb53a567788850790be4b764e225fe
Dante <denicolodante@gmail.com>
2023-04-22 00:37:21 -0300

    refactor(loading.jsx): change text from "Cargando personajes..." to "Cargando..."

--- app/characters/loading.jsx
+++ app/characters/loading.jsx
@@ -1,7 +1,7 @@
 import style from './loading.module.css'
 export default function Loading () {
   return <>
-  <p className={style.centertextonscreen}>Cargando personajes...</p>
+  <p className={style.centertextonscreen}>Cargando...</p>
   <div className={style.loader}>
     <div className={style.ldsroller}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
   </div></>
