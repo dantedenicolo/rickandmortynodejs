e6c5c95272f388c3241f2f696329a56261f8eda7
Dante <denicolodante@gmail.com>
2023-04-22 00:34:16 -0300

    feat(layout.js): add meta tags and favicon to the layout

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,13 @@ const font = Inter({ subsets: ['latin'] })
 export default function RootLayout ({ children }) {
   return (
     <html lang='en'>
+      <head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='description' content='Rick and Morty' />
+        <link rel='icon' href='/favicon.ico' />
+        <title>Rick and Morty</title>
+      </head>
       <body className={font.className}>
         <Nav />
         {children}
